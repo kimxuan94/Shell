dcfabaad9545ce8340ac390c2ff5e09c1abf2e95
unknown <toumin_o@detna-alternance.net>
2014-08-05 15:07:50 +0200

    EX2PIS01

---
+++ PIS01/Jour02/ExPIS01/Ex_2.sh
@@ -0,0 +1,17 @@
+chmod 715 test0
+touch -t 1210040000 test0
+chmod 414 test1
+touch -t 1210040000 test1
+chmod 504 test2
+touch -t 1210040000 test2
+chmod 404 test3
+touch -t 1210040000 test3
+chmod 020 test4
+touch -t 1210040000 test4
+chmod 404 test5
+touch -t 1210040000 test5
+chmod 777 test6
+touch -t 1210040000 test6
+chmod 615 testduJour
+touch -t 1210040000 testduJour
+ln -s test6 testduJour
